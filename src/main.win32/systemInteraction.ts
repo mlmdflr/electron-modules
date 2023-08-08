cc0daa0d3fb030929357af2c7b45aaa657115b38
mlmdflr <liao520ly@gmail.com>
2023-08-08 09:17:47 +0800

    3.0.5

--- src/main.win32/systemInteraction.ts
+++ src/main.win32/systemInteraction.ts
@@ -1,13 +1,7 @@
-import { execSync } from "node:child_process";
+import { exec } from "node:child_process";
 
 const rundll32exe = "rundll32.exe";
 const dllPath = "%windir%\\system3\\shimgvw.dll";
 const execParameter = ` ${rundll32exe} ${dllPath} `;
 
-export const defaultOpen = (path: string) => {
-  try {
-    return execSync(`"${path}" ${execParameter}`);
-  } catch (error) {
-    throw error;
-  }
-};
+export const defaultOpen = (path: string) => exec(`"${path}" ${execParameter}`);
