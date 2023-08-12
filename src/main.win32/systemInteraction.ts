302471f3e4b3258a1c3d31e96fc9f6a879377956
mlmdflr <liao520ly@gmail.com>
2023-08-12 18:13:00 +0800

    3.0.7

--- src/main.win32/systemInteraction.ts
+++
@@ -1,7 +0,0 @@
-import { exec } from "node:child_process";
-
-const rundll32exe = "rundll32.exe";
-const dllPath = "%windir%\\system3\\shimgvw.dll";
-const execParameter = ` ${rundll32exe} ${dllPath} `;
-
-export const defaultOpen = (path: string) => exec(`"${path}" ${execParameter}`);
